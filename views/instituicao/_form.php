99425eab2e74b976fd809057e749166bd324b75c
Marcus Vinicius <viniciusfsa@gmail.com>
2016-10-18 17:24:04 -0300

    Ajustado form de instituicao

--- views/instituicao/_form.php
+++ views/instituicao/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use \yii\helpers\ArrayHelper;
+use app\models\pais;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Instituicao */
@@ -13,12 +15,20 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'instituicao')->textInput() ?>
+    <?= $form->field($model, 'sigla')->textInput() ?>    
 
-    <?= $form->field($model, 'idPais')->textInput() ?>
+    <?= 
+    //$form->field($model, 'idPais')->textInput();
+    $form->field($model, 'idPais')->dropDownList(ArrayHelper::map(pais::find()->orderBy('nome')->all(),'id','nome'),['prompt'=>'']); 
 
-    <?= $form->field($model, 'ativo')->textInput() ?>
+    ?>
 
-    <?= $form->field($model, 'sigla')->textInput() ?>
+    <?=
+    // $form->field($model, 'ativo')->textInput()
+     $form->field($model, 'ativo')->radioList(array('1'=>'Sim',0=>'Não'));
+     ?>
+
+    
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
